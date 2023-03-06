b4553b4fbc7527083362d270c60a7393cadad3d2
Dzianis .L <leadzian213@gmail.com>
2023-03-06 15:20:45 +0300

    feat: generating data for products and stocks dynamodb tables with a script

---
+++ services/product-service/src/db/dynamodb-fill.mjs
@@ -0,0 +1,83 @@
+import path from "path"
+import { fileURLToPath } from "url"
+import dotenv from "dotenv"
+import AWS from "aws-sdk"
+import Products from "../mocks/productsMock.json" assert { type: "json" }
+import Stocks from "../mocks/stocksMock.json" assert { type: "json" }
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config({ path: path.join(__dirname, "../../.env") })
+console.info("For the region %s", process.env.REGION)
+
+// Set the region
+AWS.config.update({ region: process.env.REGION })
+
+// Create the DynamoDB service object
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
+
+ddb.listTables({ Limit: 10 }, function (err, data) {
+    if (err) {
+        console.log("Error", err.code)
+    } else {
+        console.log("Table names are ", data.TableNames)
+    }
+})
+
+// Items schemas
+
+// Products fill in
+// unable the comments to execute
+/* 
+
+const productsItems = Products.map((product) => {
+    return {
+        TableName: process.env.TABLE_PRODUCTS,
+        Item: {
+            id: { S: product.id },
+            title: { S: product.title },
+            description: { S: product.description },
+            price: {
+                N: `${product.price}`,
+            },
+        },
+    }
+})
+
+
+productsItems.forEach((productItem) => {
+    ddb.putItem(productItem, function (err, data) {
+        if (err) {
+            console.log("Error", err)
+        } else {
+            console.log("Success", data)
+        }
+    })
+})
+*/
+
+// Stocks fill in
+// Unable the comments to execute
+/*
+
+const stocksItems = Stocks.map((stock) => {
+    return {
+        TableName: process.env.TABLE_STOCKS,
+        Item: {
+            product_id: { S: stock.product_id },
+            count: { N: `${stock.count}` },
+        },
+    }
+})
+
+stocksItems.forEach((stockItem) => {
+    ddb.putItem(stockItem, function (err, data) {
+        if (err) {
+            console.log("Error", err)
+        } else {
+            console.log("Success", data)
+        }
+    })
+})
+*/
--- services/product-service/src/mocks/products.ts
+++ services/product-service/src/mocks/products.ts
@@ -1,45 +1,4 @@
 import { Stick } from "../types"
+import Products from "./productsMock.json"
 
-export const getSticksMock: () => Promise<Stick[]> = async () => [
-    {
-        description:
-            "The ProMark Rebound 5A drumstick features an acorn tip shape and a long taper, producing a rear-weighted feel that is ideal for agility.",
-        id: "MmSn-r5HPA",
-        price: 24,
-        title: "promark5a",
-    },
-    {
-        description:
-            "The ProMark Classic Forward 5B drumstick features an oval tip shape and a short taper, producing a forward-weighted feel for increased power.",
-        id: "m6Gkc19AVw",
-        price: 15,
-        title: "promark5b",
-    },
-    {
-        description:
-            'Vater Power 5A Drumsticks Features: 0.58", 16" length; Hickory; 1/2" of extra length provides extended reach and increased weight.',
-        id: "YUwynxzqQe",
-        price: 23,
-        title: "vater5a",
-    },
-    {
-        description:
-            "Balancing power, finesse, and durability, the 5B stick size is a longstanding rock favorite.",
-        id: "DyuDT2uO7N",
-        price: 15,
-        title: "vater5b",
-    },
-    {
-        description: "Vic Firth 5A American Classic Hickory.",
-        id: "7567ec4b3d",
-        price: 23,
-        title: "vicfirth5a",
-    },
-    {
-        description:
-            "Vic Firth 5B American classic drumsticks line combines tradition and Vic Firth style.",
-        id: "YPctHB693b",
-        price: 15,
-        title: "vicfirth5b",
-    },
-]
+export const getSticksMock: () => Promise<Stick[]> = async () => Products
