72ed25f570b8c621b1946f764c20a8dd6a06e19d
Dzianis .L <leadzian213@gmail.com>
2023-03-06 23:41:21 +0300

    build: packages installation

--- services/product-service/src/functions/createProduct/handler.ts
+++ services/product-service/src/functions/createProduct/handler.ts
@@ -1,13 +1,13 @@
 import { formatJSONResponse } from "@libs/api-gateway"
 import { ProductsController } from "@controllers/productsController"
 import { StickStock } from "src/types"
-import { APIGatewayEvent } from "aws-lambda"
+import { middyfy } from "@libs/lambda"
 
-export const createProduct = async (event: APIGatewayEvent) => {
-    const body = event.body
-    const stickRaw = JSON.parse(body) as Omit<StickStock, StickStock["id"]>
-    const { payload: stick, statusCode } =
-        await ProductsController.createProduct(stickRaw)
+export const createProduct = middyfy(
+    async (stickRaw: Omit<StickStock, StickStock["id"]>) => {
+        const { payload: stick, statusCode } =
+            await ProductsController.createProduct(stickRaw)
 
-    return formatJSONResponse({ payload: stick, statusCode })
-}
+        return formatJSONResponse({ payload: stick, statusCode })
+    }
+)
